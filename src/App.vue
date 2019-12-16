841ce09650572084b350ccd6ab0de8983c3f88d5
FINCTIVE <finctive@qq.com>
2019-12-16 23:43:52 +0800

    start!

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
     <md-tabs md-sync-route>
       <md-tab id="tab-home" md-label="Home" to="/" exact></md-tab>
       <md-tab id="tab-about" md-label="about" to="/about" exact></md-tab>
-      <md-tab id="tab-happy" md-label="happy" to="/happy" exact></md-tab>
     </md-tabs>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,6 @@ const routes = [{
   // this generates a separate chunk (about.[hash].js) for this route
   // which is lazy-loaded when the route is visited.
   component: () => import(/* webpackChunkName: "about" */'../views/About.vue')
-},
-{
-  path: '/happy',
-  name: 'happy',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */'../views/Happy.vue')
 }
 ]
 
