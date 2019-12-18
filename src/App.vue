eebb9bd57d62eeb9eddab7407458826973c71683
FINCTIVE <FINCTIVE@qq.com>
2019-12-19 01:09:15 +0800

    yes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app">
-    <!-- 图标字体 -->
+    <!-- 图标字体导入 -->
     <link
       href="https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.min.css"
       rel="stylesheet"
     />
-    <div class="chord-editor">
+    <div class="top-menu" md-elevation="0">
+      <md-button class="md-primary key-chooser-btn" @click="showKeyChooser = true"><md-icon>music_note</md-icon>KEY Cm</md-button>
+      <md-button class="md-primary about-btn" @click="showAbout = true">ABOUT</md-button>
+    </div>
+    <!-- About -->
+    <md-dialog-alert
+      :md-active.sync="showAbout"
+      md-content="Helloooo!"
+      md-confirm-text="Close" />
+    <md-dialog :md-active.sync="showKeyChooser">
+      <md-dialog-title>Choose Your Key</md-dialog-title>
+      <md-content>
+          <md-radio v-model="radio" :value="false">Boolean</md-radio>
+          <md-radio v-model="radio" value="my-radio">String</md-radio>
+          <md-radio v-model="radio" :value="objA">Object A</md-radio>
+          <md-radio v-model="radio" :value="objB">Object B</md-radio>
+          <md-radio v-model="radio" :value="objB">Object B</md-radio>
+          <md-radio v-model="radio">No Value</md-radio>
+          <md-radio v-model="radio" disabled>Disabled</md-radio>
+      </md-content>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showKeyChooser = false">Close</md-button>
+        <md-button class="md-primary" @click="showKeyChooser = false">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    <div class="chord-sequencer">
       <draggable
         :list="list"
         :disabled="!enabled"
@@ -46,7 +71,9 @@ export default {
         { name: 'VI', id: 6 },
         { name: 'VII', id: 7 }
       ],
-      dragging: false
+      dragging: false,
+      showAbout: false,
+      showKeyChooser: false
     }
   },
   computed: {
@@ -68,7 +95,22 @@ export default {
 .ghost-chord-card {
   opacity: 0.5;
 }
-.chord-editor {
+.top-menu {
+  position: fixed;
+  top:0;
+  width:100%;
+}
+.top-menu .key-chooser-btn{
+  text-transform: none !important;
+}
+.top-menu .about-btn{
+  float: right;
+}
+.about {
+  float : right;
+}
+.chord-sequencer {
+  padding-top: 30px;
   width: 500px;
   margin: 20px auto;
 }
